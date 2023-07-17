a5f1a9147e86ae286f20b3789d4a29f69b44b546
Amkam <amaduskamara@gmail.com>
2023-07-17 21:02:25 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Bill from "./components/Bill";
+import FriendsList from "./components/FriendsList";
+
 function App() {
   return (
-    <div className="w-full h-screen bg-slate-100">
-      <h1>Ean-N-Split</h1>
+    <div className="w-full h-screen bg-slate-100 py-24">
+      <div className="w-[900px] container mx-auto">
+        <h1 className="w-full p-6 bg-cyan-600 text-lg sm:text-1xl md:text-2xl text-slate-100">
+          Ean-N-Split
+        </h1>
+        <div className="flex gap-8 mt-10">
+          <FriendsList />
+          <Bill />
+        </div>
+      </div>
     </div>
   );
 }
