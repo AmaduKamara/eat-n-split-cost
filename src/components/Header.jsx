950aa9cafd5cb324cd170005cd4e68bfd36357df
Amkam <amaduskamara@gmail.com>
2023-07-17 22:36:53 +0000

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <h1 className="flex justify-between items-center w-full p-5 bg-white text-cyan-600 text-lg sm:text-1xl md:text-3xl">
+      <span>🍕</span> <span>Ean-N-Split</span> <span>🍔</span>
+    </h1>
+  );
+}
+
+export default Header;
