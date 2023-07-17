aa98d57971067af1731d8f5bd6d09144a7dcc28f
Amkam <amaduskamara@gmail.com>
2023-07-17 22:36:40 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Bill from "./components/Bill";
 import FriendsList from "./components/FriendsList";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="w-full h-screen bg-slate-100 py-24">
       <div className="w-[900px] container mx-auto">
-        <h1 className="w-full p-6 bg-cyan-600 text-lg sm:text-1xl md:text-2xl text-slate-100">
-          Ean-N-Split
-        </h1>
+        <Header />
         <div className="flex gap-8 mt-10">
           <FriendsList />
           <Bill />
