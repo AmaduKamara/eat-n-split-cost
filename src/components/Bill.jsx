0a7bc578854b5143f23d79467eef52b965860e13
Amkam <amaduskamara@gmail.com>
2023-07-17 22:36:45 +0000

    Update Bill.jsx

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Bill() {
   return (
-    <div className="flex-1 bg-cyan-100 px-5 py-6">
+    <div className="flex-1 bg-cyan-100 px-5 py-6 rounded-md">
       <h2 className="uppercase font-semibold text-xl text-gray-600">
         Split a Bill with John Doe
       </h2>
