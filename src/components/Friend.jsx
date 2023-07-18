aee83ee2a279bf370c32f58af67f916895c31b1c
Amkam <amaduskamara@gmail.com>
2023-07-18 20:59:44 +0000

    Update Friend.jsx

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
-function Friend() {
+function Friend({ friend }) {
   return (
-    <div className="mb-6 shadow-lg bg-white hover:bg-cyan-50 py-2 px-4 rounded-md flex items-center justify-between space-x-6">
+    <li className="mb-3 shadow-lg bg-white hover:bg-cyan-50 py-2 px-4 rounded-md flex items-center justify-between space-x-6">
       <div className="flex items-center space-x-6">
-        <div className="w-[50px] h-[50px] rounded-full shadow">
-          <img src="" alt="" />
+        <div className="w-[40px] h-[40px] rounded-full shadow overflow-hidden">
+          <img src={friend.image} alt={friend.name} />
         </div>
         <div>
-          <h3 className="font-semibold">John Doe</h3>
+          <h3 className="font-semibold">{friend.name}</h3>
           <p className="text-xs">John Doe owes you</p>
         </div>
       </div>
-      <button className="py-2 px-4 rounded-md bg-cyan-600 text-slate-50 hover:bg-cyan-700 transition duration-300">
+      <button className="py-1 px-4 rounded-md bg-cyan-600 text-slate-50 hover:bg-cyan-700 transition duration-300">
         Select
       </button>
-    </div>
+    </li>
   );
 }
 
