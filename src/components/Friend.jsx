082bb5dd828454381c3fe7c26e2b49d86aa68a42
Amkam <amaduskamara@gmail.com>
2023-07-18 20:27:58 +0000

    Update Friend.jsx

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Friend() {
   return (
-    <div className="mb-6 shadow-lg bg-white py-2 px-4 rounded-md flex items-center justify-between space-x-6">
+    <div className="mb-6 shadow-lg bg-white hover:bg-cyan-50 py-2 px-4 rounded-md flex items-center justify-between space-x-6">
       <div className="flex items-center space-x-6">
         <div className="w-[50px] h-[50px] rounded-full shadow">
           <img src="" alt="" />
