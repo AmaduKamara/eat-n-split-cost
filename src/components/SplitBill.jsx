90ec43b9aacdb25926c51188f507b653c5f120d0
Amkam <amaduskamara@gmail.com>
2023-07-21 12:25:07 +0000

    Update SplitBill.jsx

--- src/components/SplitBill.jsx
+++ src/components/SplitBill.jsx
@@ -3,7 +3,7 @@ import BillForm from "./BillForm";
 
 function SplitBill() {
   return (
-    <div className="mx-6 bg-cyan-100 px-5 py-6 rounded-md border-l-4 border-cyan-600 h-[340px] mb-16">
+    <div className="mx-6 bg-cyan-100 px-5 py-6 rounded-md border-l-4 border-cyan-600 h-[360px] mb-16">
       <h2 className="uppercase font-semibold text-xl text-gray-600">
         Split a Bill with John Doe
       </h2>
