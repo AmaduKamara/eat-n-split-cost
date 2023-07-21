f98d182b895f547d37ef67cb6e6030ad458dc059
Amkam <amaduskamara@gmail.com>
2023-07-21 12:15:23 +0000

    Update BillForm.jsx

--- src/components/BillForm.jsx
+++ src/components/BillForm.jsx
@@ -5,38 +5,46 @@ function BillForm() {
   return (
     <form className="w-full bg-cyan-100 px-3 mt-5">
       <div className="mb-3 flex items-center space-x-4 justify-between">
-        <label htmlFor="billValue">💰 Bill value</label>
+        <label className="sm:text-xs" htmlFor="billValue">
+          💰 Bill value
+        </label>
         <input
-          className="py-2 px-4 w-[220px] border rounded-md focus:outline-cyan-600"
+          className="py-1 px-4  w-[150px] border rounded-md focus:outline-cyan-600"
           type="text"
           placeholder="$ Bill amount"
           id="billValue"
         />
       </div>
       <div className="mb-3 flex items-center space-x-4 justify-between">
-        <label htmlFor="yourExpense">🧍🏻‍♀️ Your expense</label>
+        <label className="sm:text-xs" htmlFor="yourExpense">
+          🧍🏻‍♀️ Your expense
+        </label>
         <input
-          className="py-2 px-4 w-[220px] border rounded-md focus:outline-cyan-600"
+          className="py-1 px-4  w-[150px] border rounded-md focus:outline-cyan-600"
           type="text"
           placeholder="Your expense"
           id="yourExpense"
         />
       </div>
       <div className="mb-3 flex items-center space-x-4 justify-between">
-        <label htmlFor="friend">🧍🏻 John expense</label>
+        <label className="sm:text-xs" htmlFor="friend">
+          🧍🏻 John expense
+        </label>
         <input
-          className="py-2 px-4 w-[220px] border rounded-md focus:outline-cyan-600"
+          className="py-1 px-4  w-[150px] border rounded-md focus:outline-cyan-600"
           type="text"
-          placeholder="Your friend's expense"
+          placeholder="$"
           id="friend"
           disabled
         />
       </div>
       <div className="mb-3 flex items-center space-x-4 justify-between">
-        <label htmlFor="whoIsPaying">🧍🏻🤑 Who is paying</label>
+        <label className="sm:text-xs" htmlFor="whoIsPaying">
+          🧍🏻🤑 Who is paying
+        </label>
 
         <select
-          className="py-2 px-3 w-[220px] border rounded-md focus:outline-cyan-600"
+          className="py-1 px-3 w-[150px] border rounded-md focus:outline-cyan-600"
           id="whoIsPaying"
         >
           <option value="You">You</option>
@@ -46,7 +54,7 @@ function BillForm() {
       <div className="flex justify-end">
         <button
           onClick={handleSubmit}
-          className="py-2 px-10 border border-cyan-600 rounded-md bg-cyan-600 text-slate-50 hover:bg-cyan-700 transition duration-300"
+          className="py-1 px-12 border border-cyan-600 rounded-md bg-cyan-600 text-slate-50 hover:bg-cyan-700 transition duration-300"
         >
           Add Bill
         </button>
