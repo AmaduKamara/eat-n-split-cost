652d6b95d19ae2d224a6660786b7bc0ed46ea570
Amkam <amaduskamara@gmail.com>
2023-07-21 12:15:28 +0000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 function Header() {
   return (
-    <h1 className="flex justify-between items-center w-full p-5 bg-white text-cyan-600 text-lg sm:text-1xl md:text-3xl">
-      <span>🍕</span> <span>Ean-N-Split</span> <span>🍔</span>
-    </h1>
+    <header className="sticky top-0">
+      <h1 className="flex justify-between items-center w-full py-5 px-6 bg-white text-cyan-600 text-lg sm:text-1xl md:text-3xl">
+        <span>🍕</span> <span>Ean-N-Split</span> <span>🍔</span>
+      </h1>
+    </header>
   );
 }
 
