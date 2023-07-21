3344ee6f4921167e1d03b6e339b679f4b67619de
Amkam <amaduskamara@gmail.com>
2023-07-21 09:46:27 +0000

    Update FriendsList.jsx

--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -1,36 +1,7 @@
-import React, { useState } from "react";
 import Friend from "./Friend";
 import AddFriend from "./AddFriend";
 
-const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Amkam",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
-
-function FriendsList() {
-  const [showForm, setShowForm] = useState(false);
-  const friends = initialFriends;
-
-  const closeAddFriendForm = () => {
-    setShowForm(false);
-  };
-
+function FriendsList({ friends, onCloseForm, showForm }) {
   return (
     <div className="flex-1">
       <ul>
@@ -42,14 +13,14 @@ function FriendsList() {
       {!showForm && (
         <div className="flex justify-end">
           <button
-            onClick={() => setShowForm(true)}
+            onClick={onCloseForm}
             className="flex-end items-end py-2 mt-2 px-8 border border-cyan-600 text-cyan-600 uppercase rounded-md hover:bg-cyan-700 hover:text-slate-50 transition duration-300"
           >
             Add friend
           </button>
         </div>
       )}
-      {showForm && <AddFriend onCloseForm={closeAddFriendForm} />}
+      {showForm && <AddFriend onCloseForm={onCloseForm} />}
     </div>
   );
 }
