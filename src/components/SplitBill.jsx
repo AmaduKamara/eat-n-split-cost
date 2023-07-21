68ebfa03f4f522b953bced1393680e8794394bbf
Amkam <amaduskamara@gmail.com>
2023-07-21 13:07:22 +0000

    Update SplitBill.jsx

--- src/components/SplitBill.jsx
+++ src/components/SplitBill.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import BillForm from "./BillForm";
 
-function SplitBill() {
+function SplitBill({ selectedFriend }) {
   return (
     <div className="mx-6 bg-cyan-100 px-5 py-6 rounded-md border-l-4 border-cyan-600 h-[360px] mb-16">
       <h2 className="uppercase font-semibold text-xl text-gray-600">
-        Split a Bill with John Doe
+        Split a Bill with {selectedFriend.name}
       </h2>
-      <BillForm />
+      <BillForm selectedFriend={selectedFriend} />
     </div>
   );
 }
