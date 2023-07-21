d8fb66b5d8a3ee047b764f48bb8e16e0a5083e4c
Amkam <amaduskamara@gmail.com>
2023-07-21 13:07:17 +0000

    Update Friend.jsx

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
-function Friend({ friend }) {
+function Friend({ friend, onSelection, selectedFriend }) {
+  const isSelected = selectedFriend?.id === friend?.id;
+
   return (
-    <li className="mb-3 shadow-lg bg-white hover:bg-cyan-50 transition-all duration-300 py-2 px-4 rounded-md flex items-center justify-between space-x-6">
+    <li
+      className={`${
+        isSelected
+          ? "bg-cyan-100 mb-3 shadow-lg hover:bg-cyan-50 transition-all duration-300 py-2 px-4 rounded-md flex items-center justify-between space-x-6"
+          : "bg-cyan-100 mb-3 shadow-lg bg-white hover:bg-cyan-50 transition-all duration-300 py-2 px-4 rounded-md flex items-center justify-between space-x-6"
+      } `}
+    >
       <div className="flex items-center space-x-6">
         <div className="w-[40px] h-[40px] rounded-full shadow overflow-hidden">
           <img src={friend.image} alt={friend.name} />
@@ -20,11 +28,16 @@ function Friend({ friend }) {
             </p>
           )}
           {friend.balance === 0 && (
-            <p className="text-xs md:text-lg">You and {friend.name} are even </p>
+            <p className="text-xs md:text-lg">
+              You and {friend.name} are even{" "}
+            </p>
           )}
         </div>
       </div>
-      <button className="py-1 md:py-2 px-4 rounded-md bg-cyan-600 text-slate-50 hover:bg-cyan-700 transition duration-300">
+      <button
+        onClick={() => onSelection(friend)}
+        className="py-1 md:py-2 px-4 rounded-md bg-cyan-600 text-slate-50 hover:bg-cyan-700 transition duration-300"
+      >
         Select
       </button>
     </li>
