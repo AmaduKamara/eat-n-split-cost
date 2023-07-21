7e72e718131bf0c2603695fe2979f231892c1195
Amkam <amaduskamara@gmail.com>
2023-07-21 10:17:26 +0000

    Update AddFriend.jsx

--- src/components/AddFriend.jsx
+++ src/components/AddFriend.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddFriend() {
+function AddFriend({ onAddFriend }) {
+  const [name, setName] = useState("");
+  const [balance, setBalance] = useState(0);
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const id = 123890;
+
+    const newFriend = {
+      id,
+      name,
+      image: image + `?u="${id}"`,
+      balance,
+    };
+
+    if (name === "" || image === "") return;
+
+    onAddFriend(newFriend);
+
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+    setBalance(0);
   };
 
   return (
@@ -14,6 +35,8 @@ function AddFriend() {
           type="text"
           placeholder="Friend name"
           id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
       </div>
       <div className="mb-3 flex items-center space-x-4 justify-between">
@@ -21,24 +44,18 @@ function AddFriend() {
         <input
           className="py-1 px-4 border rounded-md focus:outline-cyan-600 w-64"
           type="text"
-          value="https://i.pravatar.cc/48"
+          value={image}
           id="photo"
+          onChange={(e) => setImage(e.target.value)}
         />
       </div>
       <div className="flex justify-end items-center">
-        <div>
-          <button className="py-1 mr-4 px-8 rounded-md border border-cyan-600 text-cyan-600 hover:bg-cyan-700 hover:text-slate-100 cursor-pointer transition duration-300">
-            Cancel
-          </button>
-        </div>
-        <div>
-          <button
-            onClick={handleSubmit}
-            className="py-1 px-6 border border-cyan-600 rounded-md bg-cyan-600 text-slate-50 hover:bg-cyan-700 transition duration-300"
-          >
-            Add Friend
-          </button>
-        </div>
+        <button
+          onClick={handleSubmit}
+          className="py-1 px-6 border border-cyan-600 rounded-md bg-cyan-600 text-slate-50 hover:bg-cyan-700 transition duration-300"
+        >
+          Add Friend
+        </button>
       </div>
     </form>
   );
